8452ab8249b86ec5d88eda71064999d36cbc7d2b
dancoe77 <dan@shoeman-tech.com>
2023-11-10 01:09:39 -0800

    added cart fuctionality

--- Real_Project_Example_Git_and_GitHub/01-starting+code/starting_code/src/App.js
+++ Real_Project_Example_Git_and_GitHub/01-starting+code/starting_code/src/App.js
@@ -19,8 +19,7 @@ function showCartHandler() {
 
   return (
     <CartProvider>
-        {cartIsShown && <div> Cart...</div>}
-      <Cart />
+        {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
