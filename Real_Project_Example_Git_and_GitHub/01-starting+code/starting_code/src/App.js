f2dc91e9a424945ef98a92f72aedf6eaa063af6b
dancoe77 <dan@shoeman-tech.com>
2023-11-10 00:46:19 -0800

    added cart visibility state

--- Real_Project_Example_Git_and_GitHub/01-starting+code/starting_code/src/App.js
+++ Real_Project_Example_Git_and_GitHub/01-starting+code/starting_code/src/App.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  useState();
+const [cartIsShown,setCartIsShown] = useState(false);
+
+function showCartHandler() {
+  setCartIsShown(true)
+}
+
+  function hideCartHandler () {
+  setCartIsShown(false)
+}
+
   return (
     <CartProvider>
-      <Header />
+        {cartIsShown && <div> Cart...</div>}
+      <Cart />
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
